37982fc6a5da5b79dfc377bc21c635ae6a9bb93e
SrLucca <luccatsoares@gmail.com>
2022-09-17 19:17:37 -0300

    modelo cadastro empresa

--- CMproject/company/models.py
+++ CMproject/company/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class companyModel(models.Model):
+    nome = models.CharField(max_length=255, blank=False, null=False)
+    cnpj = models.CharField(max_length=14, blank=False, null=False)
+    email = models.EmailField(blank=False, null=False)
+    telefone = models.CharField(max_length=11, help_text="Ex: 11999999999")
+    endereco = models.TextField(blank=False, null=False, help_text="Rua,numero,cep,complemento etc..")
