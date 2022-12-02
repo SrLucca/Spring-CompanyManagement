b840050d5b2df93a76fedcfd2c28e29cc25b77da
Lucca Torrezilha Soares <luccatsoares@gmail.com>
2022-12-02 15:47:28 -0300

    construcao do calendario dinamico

--- SprintProject/employeesCalendar/urls.py
+++ SprintProject/employeesCalendar/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from employeesCalendar import views
+
 
 urlpatterns = [
-    
+    path('calendario/<int:year>/<str:month>/', views.calendarView, name='calendar'),
 ]
--- SprintProject/employeesCalendar/views.py
+++ SprintProject/employeesCalendar/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+import calendar
+from calendar import HTMLCalendar
 # Create your views here.
+
+def calendarView(request, year, month):
+
+    month_number = list(calendar.month_name).index(month)
+
+    return render(request,'calendar.html',{
+        "year":year,
+        "month":month,
+        "month_number":month_number
+    })
